804fb4bddfa228501de117fa5b129cbdbbba6258
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-02-27 21:23:34 +0600

    explore some basic javaScript function structure

---
+++ function.js
@@ -0,0 +1,81 @@
+// const name = "Sifat";
+// const age = 22;
+// const address = "Dhaka, Bangladesh";
+// const professions = "Student";
+
+// console.log(name, age, address, professions);
+
+// Function Syntax
+
+/*
+
+function function_name(){
+
+function body
+
+}
+
+function call()
+
+*/
+
+// function showName() {
+//   console.log("Sifat");
+// }
+
+// showName();
+
+// function showName(name, age, profession) {
+//   console.log(name, age, profession);
+// }
+
+// showName("SIfat", 22, "Student");
+
+// function showValues() {
+//   console.log("Polao");
+//   console.log("Goru");
+//   console.log("Murgi");
+// }
+
+// showValues();
+
+// function plus(x, y) {
+//   const total = x + y;
+//   console.log(total);
+// }
+
+// plus(5, 15);
+// plus(50, 105);
+
+// function giveName() {
+//   const name = "Sifat";
+//   return name;
+// }
+
+// const nameStore = giveName();
+
+// console.log(nameStore);
+
+// Type of Function ***
+
+// 1. No input (parameter) No return
+// function show() {
+//   console.log("No parameter no input");
+// }
+
+// 2. parameter but no return
+// function name(x, y) {
+//   console.log(x + y);
+// }
+
+// 3. no parameter but return
+// function para() {
+//   const name = "Dhaka";
+//   return name;
+// }
+
+// 4. Parameters with return
+// function digits(x) {
+//   const double = x * 2;
+//   return double;
+// }
