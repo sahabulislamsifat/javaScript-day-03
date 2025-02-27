1fdb66285f26c0fb173d06044bccfe2f5f014f67
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-02-28 01:57:27 +0600

    explore some problem of javaScript array

---
+++ problemOne.js
@@ -0,0 +1,81 @@
+// Amar kache ekta ARRAY te kichu numbers ache, amake ber korte hobe sei array te thaka sob cheye boro digits konta.
+
+// const numbers = [1, 2, 3, 4, 54, 6, 7, 8, 9, 10];
+
+/*
+Which one is the biggest numbers
+
+biggest = 51
+biggest = 45
+biggest = 24
+biggest = 99
+biggest = 154
+biggest = 2565
+biggest = 5454
+biggest = 2156
+
+*/
+
+// let biggest = numbers[0];
+// console.log("Before Loop :", biggest);
+
+// for (let i = 1; i < numbers.length; i++) {
+//   console.log("Current number", numbers[i], "current biggest:", biggest);
+//   if (numbers[i] > biggest) {
+//     biggest = numbers[i];
+//   }
+//   console.log("Current biggest :", biggest);
+// }
+
+// console.log("After loop", biggest);
+
+// find biggest number
+// const maxNumber = Math.max(numbers);
+// const maxNumber = Math.max(...numbers);
+
+// console.log(maxNumber);
+
+// find Minimum number
+// const findMin = Math.min(...numbers);
+
+// console.log(findMin);
+
+// function giveBiggestNumber(array) {
+//   if (!Array.isArray(array)) {
+//     // console.log("this is an array......");
+//     return "this is not an array......";
+//   }
+
+//   let biggest = 0;
+//   for (let i = 1; i < array.length; i++) {
+//     if (array[i] > biggest) {
+//       biggest = array[i];
+//     }
+//   }
+//   return biggest;
+// }
+
+// const numbers = [1, 2, 3, 4, 54, 6, 7, 8, 9, 10];
+// const numbers = [1, 2, 33, 4, 54, 6, 7, 78, 9, 10];
+// const numbers = "String";
+// const numbers = true;
+// const result = giveBiggestNumber(numbers);
+
+// console.log(result);
+
+// function giveSmallestNumber(numbers) {
+//   let smallest = numbers[0];
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] < smallest) {
+//       smallest = numbers[i];
+//     }
+//     return smallest;
+//   }
+// }
+
+// const numbers = [1, 2, 33, 4, 54, 6, 7, 78, 9, 10];
+
+// const result = giveSmallestNumber(numbers);
+
+// console.log(result);
+
